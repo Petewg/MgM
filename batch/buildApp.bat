f20fb424955bf690dda7c8448cb75b2dbd855f7e
Pete <pete_west@yahoo.gr>
2016-12-24 14:58:03 +0200

    upload batch files

---
+++ batch/buildApp.bat
@@ -0,0 +1,73 @@
+@ECHO OFF
+
+:: ECHO Compiling %1 sources 
+
+CALL %~dp0SetPaths.bat
+SET RUNEXE=-run
+SET STRIP=-strip
+
+:: Compile Resources
+   IF EXIST _temp.rc DEL _temp.rc   > NUL
+   
+	ECHO #define HMGRPATH %MGMPATH%\RESOURCES > _hmg_resconfig.h
+   
+	IF EXIST *.rc COPY /V /A *.rc PRJ_RES_.rc  > NUL
+	
+   COPY /V /A %MGMPATH%\resources\hmg.rc + %MGMPATH%\resources\filler + PRJ_RES_.rc + %MGMPATH%\resources\filler _temp.rc  > NUL
+   
+	IF EXIST PRJ_RES_.rc DEL PRJ_RES_.rc > NUL
+   
+	windres -i _temp.rc -o _temp.o  2> _BuildLog.txt
+   
+   :: DEL _hmg_resconfig.h > NUL
+
+IF "%2"=="-c" GOTO Console
+
+IF "%2"=="-norun" SET RUNEXE=-run-
+IF "%2"=="-norun" SHIFT /2
+
+:Gui
+      hbmk2 -n -mt -cpu=x86 -lang=en -w3 %RUNEXE% -ge1 -ql %STRIP% -jobs=2 -D__CALLDLL__ %1 %2 %3 %4 %5 %6 %7 %MGMPATH%\minigui.hbc 2>> _BuildLog.txt
+   :: hbmk2 -n -cpu=x86 -lang=en -w3 %RUNEXE% -ge1 -ql %STRIP%               %1 %2 %3 %4 %5 %6 %7 %MGMPATH%\minigui.hbc 2>> _BuildLog.txt
+   GOTO Check
+
+:Console
+   SHIFT /2 
+   IF "%2"=="-norun" SET RUNEXE=-run-
+   IF "%2"=="-norun" SHIFT /2
+   ::hbmk2 -n -cpu=x86 -lang=en %RUNEXE% -ql -info -exitstr %STRIP% -D__CALLDLL__ %1 _temp.o %2 %3 %4 %5 %6 %7 2>> _BuildLog.txt
+   hbmk2 -n -mt -cpu=x86 -lang=en -w3 %RUNEXE% -ge1 -ql %STRIP% -jobs=2 -D__CALLDLL__ %1 %2 %3 %4 %5 %6 %7 %MGMPATH%\minigui.hbc 2>> _BuildLog.txt
+   
+
+:Check
+   IF ERRORLEVEL 1 GOTO Err
+   GOTO Run
+
+:Err
+   ECHO 
+   ECHO Build Error(s) occured. Inspect _BuildLog.txt to see what failed..
+   START _BuildLog.txt
+   PAUSE > NUL
+   GOTO Quit
+
+:Run
+   IF "%2"=="-norun" GOTO Quit
+	IF "%RUNEXE%"=="-run-" GOTO Quit
+   ECHO  
+   ECHO Running %1 ...
+   ECHO ----------------------------------------------
+   ECHO When end running, press a key to delete %1.exe
+   PAUSE > NUL
+   IF EXIST %1.exe DEL %1.exe
+
+
+:Quit
+   :: Clean up
+   SET MGMPATH=
+   DEL _hmg_resconfig.h
+   DEL _temp.* > NUL
+   DEL _BuildLog.txt > NUL
+   IF EXIST %1.ppo DEL %1.ppo > NUL
+   IF EXIST ErrorLog.htm DEL ErrorLog.htm > NUL
+
+	
---
+++ batch/buildOtherLibs.bat
@@ -0,0 +1,121 @@
+REM @echo off
+
+@CALL %~dp0SetPaths.bat
+
+SET SRC=%~d0\MiniguiM\minigui\source
+
+echo ------------------------------------------------------------------------- >> Build.log
+echo Build Other Libs started on %date% %time% >> Build.log
+echo ------------------------------------------------------------------------- >> Build.log
+echo    >>  Build.log
+
+echo    >>  Build.log
+echo === HBPRINTER (%date% %time%) === >> Build.log
+CD /d %SRC%\HbPrinter
+hbmk2 -ql winprint.hbp   2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === MINIPRINT (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\MiniPrint
+hbmk2 -ql miniprint.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === MINIPRINT-2 (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\MiniPrint2
+hbmk2 -ql miniprint.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === TSBROWSE (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\TsBrowse
+hbmk2 -ql tsb.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === ADORDD (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\AdoRDD
+hbmk2 -ql adordd.hbp      2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === BOSTAURUS (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\BosTaurus
+hbmk2 -ql bostaurus.hbp   2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === CALLDLL (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\CallDll
+hbmk2 -ql calldll.hbp   2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === HBVPDF (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\HbVpdf
+hbmk2 -ql hbvpdf   2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === HBXML (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\HbXML
+hbmk2 -ql hbxml   2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === HBZIPARC (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\HbZipArc
+hbmk2 -ql ziparc.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === PROPGRID (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\PropGrid
+hbmk2 -ql propgrid.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === PROPSHEET (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\PropSheet
+hbmk2 -ql propsheet.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === QHTM (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\QHTM
+hbmk2 -ql qhtm.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === SOCKET (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\Socket
+hbmk2 -ql socket.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === TMSAGENT (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\TMsAgent
+hbmk2 -ql msagent.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === WINREPORT (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\WinReport
+hbmk2 -ql winreport.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === WINPROCESS (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\WinProcess
+hbmk2 -ql winprocess.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === HMG_HPDF (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\HMG_HPDF
+hbmk2 -ql hmg_hpdf.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === HBAES (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\hbAES
+hbmk2 -ql hbaes.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === DEBUGGER (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\Debugger
+hbmk2 -ql debugger.hbp    2>> %~dp0Build.log
+
+echo    >>  %~dp0Build.log
+echo === HbOLE (%date% %time%) === >> %~dp0Build.log
+CD /d %SRC%\HbOLE
+hbmk2 -ql hbole.hbp    2>> %~dp0Build.log
+
+echo   >> %~dp0Build.log
+echo Build Other Libs finished on %date% %time% >> %~dp0Build.log
+
+SET SRC=
+cd %~dp0
